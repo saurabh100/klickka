1c74a362afac79f4f93845120a6e6d3fa981ac45
saurabh <saurabh.upadhyay@webdew.in>
2019-02-02 11:46:48 +0530

    roles and permissions

--- app/User.php
+++ app/User.php
@@ -3,12 +3,11 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use App\Permissions\HasPermissionsTrait;
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasPermissionsTrait;
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (\Illuminate\Http\Request $request) {
+
+	$user = $request->user();
+//	dd($user);
+
+//	dd($user->hasRole('developer'));
+//		dd($user->givePermissionsTo('create-tasks'));
+	dd($user->can('create-tasks'));
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::group(['middleware' => 'role:admin'], function() {
+////	Route::group(['middleware' => 'role:admin,edit-posts'], function() {});
+//
+//		Route::get('/admin', function() {
+//		return 'Welcome Admin';
+//	});
+//});
